e488ee1479702dc952ec57494379e87d924f1dba
khammal <karam.hammal@gmail.com>
2025-03-26 22:30:00 -0400

    Upload backend files

---
+++ backend/src/main/java/com/uw/examscheduler/UWExamSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.uw.examscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UWExamSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UWExamSchedulerApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/uw/examscheduler/exam/repo/ExamRepository.java
@@ -0,0 +1,14 @@
+package com.uw.examscheduler.exam.repo;
+
+import com.uw.examscheduler.exam.model.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExamRepository extends JpaRepository<Exam, String> {
+    Optional<Exam> findByCourseAndSection(String course, String section);
+
+    void deleteByCourseAndSection(String course, String section);
+}
